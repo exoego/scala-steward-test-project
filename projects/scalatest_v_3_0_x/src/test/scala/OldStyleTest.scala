b393053a7e99903810603f9e3adf4e2e4f3a3a90
Scala Steward <ytatsuno.jp@gmail.com>
2020-10-18 15:03:31 +0900

    Update scalatest to 3.1.4

--- projects/scalatest_v_3_0_x/src/test/scala/OldStyleTest.scala
+++ projects/scalatest_v_3_0_x/src/test/scala/OldStyleTest.scala
@@ -1,10 +1,11 @@
-import org.scalatest.FunSuite
+
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.funsuite.AnyFunSuite
 
 /**
  * Foo Bar
  */
-class OldStyleTest extends FunSuite with GeneratorDrivenPropertyChecks {
+class OldStyleTest extends AnyFunSuite with GeneratorDrivenPropertyChecks {
   test("case 1") {
     forAll { (s: String) =>
       assert(s.length == s.size)
